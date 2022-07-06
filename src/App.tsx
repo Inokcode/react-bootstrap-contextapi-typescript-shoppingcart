f269b3d1b63bbd18b1ae72d893ec39c12e793829
Inokcode <regilearn@gmail.com>
2022-07-06 15:44:16 +0530

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,21 @@ import Home from "./components/Home";
 import Store from "./components/Store";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Container className="mb-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="store" element={<Store />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-      </Container>
+      <ShoppingCartProvider>
+        <NavBar />
+        <Container className="mb-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="store" element={<Store />} />
+            <Route path="about" element={<About />} />
+          </Routes>
+        </Container>
+      </ShoppingCartProvider>
     </>
   );
 }
